cf0592183e213be873d2e96847f7796af82e08da
Joseph Tongay <dirk.tongay@gmail.com>
2017-01-05 15:25:41 -0600

    fix headers to accept cors

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ const users = require('./routes/users.route')
 // app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Use Routes
 
